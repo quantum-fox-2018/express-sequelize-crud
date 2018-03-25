ced2f5437ad1c4c64ca3527d3e95f737e56bd4d7
yasirmaulana <yasir.maulana@gmail.com>
2018-03-25 17:32:58 +0700

    menambahkan fitur add student, teacher, subject

--- app.js
+++ app.js
@@ -19,25 +19,59 @@ app.get('/teachers',function(req,res){
     })
   })
   .catch(err => {res.send(err)})
+})
 
 // add data teachers
-app.get('/teachers/addTeacher',function(){
-  res.render('./teachers/addTeacher')
+app.get('/teachers/add',function(req,res){
+  res.render('teachers/addTeacher')
 })
 
-app.get('/teachers/edit/:id',function(req,res){
-  res.render('./teachers/edit')
+app.post('/teachers/add',function(req,res){
+  model.Teacher.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+  .then(added => {
+    // let alert = 'the Data have inputed...'
+    res.redirect('/teachers' )
+  })
+  .catch(err => {res.send(err)})
 })
 
-})
+// app.get('/teachers/edit/:id',function(req,res){
+//   res.render('./teachers/edit')
+// })
+
 
 // =========================================== Subjects
+// show data subject
 app.get('/subjects',function(req,res){
   model.Subject.findAll({raw:true})
   .then(Subjects => {
     res.render('subjects',{
       Subjects:Subjects
     })
+  })
+  .catch(err => {res.send(err)})
+})
+
+// add data subject
+app.get('/subject/add',function(req,res){
+  res.render('subjects/addSubject')
+})
+
+app.post('/subject/add',function(req,res){
+  model.Subject.create({
+    subject_name: req.body.subject_name,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+  .then(added => {
+    // let alert = 'the Data have inputed...'
+    res.redirect('/subjects' )
   })
   .catch(err => {res.send(err)})
 })
@@ -49,6 +83,24 @@ app.get('/students', function (req, res){
     res.render('students',{
       Students:Students
     })
+  })
+  .catch(err => {res.send(err)})
+})
+
+app.get('/students/add',function(req, res){
+  res.render('students/addStudent')
+})
+
+app.post('/students/add',function(req, res){
+  model.Student.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+  .then(added => {
+    res.redirect('/students')
   })
   .catch(err => {res.send(err)})
 })
